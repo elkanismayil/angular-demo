75f963042610a029b27c95723e2d2305b206f6d8
Elxan <elxan310@gmail.com>
2021-04-08 13:19:00 +0400

    commit 3

---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,28 @@
+import { CartItems } from './../models/cartItems';
+import { User } from './../models/user';
+import { Injectable } from '@angular/core';
+import { CartItem } from '../models/cartItem';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  constructor() {}
+
+  addToCart(user: User) {
+    let item = CartItems.find((c) => c.user.userId === user.id);
+
+    if (item) {
+      item.quantity++;
+    } else {
+      let cartItem = new CartItem();
+      cartItem.user = user;
+      cartItem.quantity = 1;
+      CartItems.push(cartItem);
+    }
+  }
+
+  list(): CartItem[] {
+    return CartItems;
+  }
+}
